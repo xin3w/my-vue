fc43b0514145b87a4152201147c7cf79d12d9f75
zhanghui <xin3w@126.com>
2019-09-23 17:01:57 +0800

    安装less
    具体配置webpack.base.conf.js，具体全局引用app.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -80,6 +80,7 @@
         </a>
       </li>
     </ul>
+    <div class="test">test</div>
   </div>
 </template>
 
@@ -98,21 +99,3 @@ export default {
   methods: {}
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
